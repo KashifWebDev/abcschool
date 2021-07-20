c98810cd02841cd61237183fcce2e487874f85b7
kmali <kmalik748@gmail.com>
2021-07-20 14:21:12 +0500

    1

--- admin_student_registration.php
+++ admin_student_registration.php
@@ -204,31 +204,30 @@ https://github.com/silviomoreto/bootstrap-select/issues/1135
                         $fb = $_POST["fb"];
                         $insta = $_POST["insta"];
                         $email = $_POST["email"];
-                        $pic = "";
+                        $pic = "default.jpg";
 
-                        $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt'); // valid extensions
-                        $path = 'img/students/'; // upload directory
+                        if (!empty($_FILES["image"]["name"])) {
+                            $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp', 'pdf', 'doc', 'ppt'); // valid extensions
+                            $path = 'img/students/'; // upload directory
 
                             $img = $_FILES['image']['name'];
                             $tmp = $_FILES['image']['tmp_name'];
 // get uploaded file's extension
                             $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
 // can upload same image using rand function
-                            $final_image = rand(1000,1000000).$img;
+                            $final_image = rand(1000, 1000000) . $img;
 // check's valid format
-                            if(in_array($ext, $valid_extensions))
-                            {
-                                $path = $path.strtolower($final_image);
-                                if(move_uploaded_file($tmp,$path))
-                                {
-                                   $pic = $final_image;
+                            if (in_array($ext, $valid_extensions)) {
+                                $path = $path . strtolower($final_image);
+                                if (move_uploaded_file($tmp, $path)) {
+                                    $pic = $final_image;
                                 }
+                            } else {
+                                echo 'invalid';
+                                exit();
+                                die();
                             }
-                            else
-                            {
-                                echo 'invalid'; exit(); die();
-                            }
-
+                        }
                         $sql = "INSERT INTO master_registration_list (email, student_id, registration_invoice_no, registration_date, student_name,
                                       country, passport_no, dob, phone_no, guardian_contact, address_S_A, facebook, insta, pic) VALUES 
                                 ('$email', '$student_id', '$invoice_num', '$registration_date', '$name', '$country', '$passport',
