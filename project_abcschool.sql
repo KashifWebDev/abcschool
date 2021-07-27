bb3de5ac092edf6990c21495d71930ee0c6220aa
kmali <kmalik748@gmail.com>
2021-07-27 06:16:40 +0500

    1

--- project_abcschool.sql
+++ project_abcschool.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2021 at 05:12 AM
+-- Generation Time: Jul 27, 2021 at 03:14 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
