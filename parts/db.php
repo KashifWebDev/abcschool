8c43404a0d3eaa5b03227efe86cb3b564b8d0805
kmali <kmalik748@gmail.com>
2021-09-17 11:43:54 +0500

    a

--- parts/db.php
+++ parts/db.php
@@ -2,7 +2,7 @@
 
 //$con = mysqli_connect("localhost","root","","project_abcschool");
 
-$con = mysqli_connect("localhost","j18jocnn_18joris","CPvvGapgy)Oy","j18jocnn_18jor");
+$con = mysqli_connect("localhost","abcdaevx_abc","3}t{Zt?x.Y3)","abcdaevx_abcDatabase");
 
 // Check connection
 if (mysqli_connect_errno()) {
