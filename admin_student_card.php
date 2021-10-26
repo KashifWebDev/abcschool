a2443e25556e6de33b197591caa30111e82b4e2e
kmali <kmalik748@gmail.com>
2021-10-26 16:46:19 +0500

    a

--- admin_student_card.php
+++ admin_student_card.php
@@ -19,7 +19,7 @@ if(isset($_GET["mail"])){
     $txt .= "Kind Regards,\n";
     $txt .= "ABC International Admin Team";
 
-    echo $txt; exit(); die();
+
 
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
