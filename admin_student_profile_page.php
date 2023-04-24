7236df67842f76ea8632b102eb9fc49babf80531
kashif <admin@kashifali.me>
2023-04-24 09:10:10 +0500

    Added 2023 year on the profile page

--- admin_student_profile_page.php
+++ admin_student_profile_page.php
@@ -222,6 +222,7 @@ require 'parts/head.php';
                                                 <option>-- SELECT --</option>
                                                 <option value="2021">2021</option>
                                                 <option value="2022">2022</option>
+                                                <option value="2023">2023</option>
                                             </select>
                                         </div>
                                         <div class="row">
